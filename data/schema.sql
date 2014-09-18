07613b93553be993ce3886bccb3e124ac352ba9e
David Persson <davidpersson@gmx.de>
2014-09-18 17:58:45 +0200

    Adding schema.

---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `ecommerce_vouchers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) NOT NULL DEFAULT '',
+  `type` varchar(20) NOT NULL,
+  `uses_left` int(11) DEFAULT '1' COMMENT 'never allow infinite uses',
+  `expires` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
