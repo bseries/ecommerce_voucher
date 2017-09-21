f5df4e8d6c54f3701cd6df3f4eccb6369236491c
David Persson <nperson@gmx.de>
2017-09-21 16:02:42 +0200

    Reorder vouchers.

--- config/panes.php
+++ config/panes.php
@@ -23,7 +23,7 @@ extract(Message::aliases());
 Panes::register('ecommerce.vouchers', [
 	'title' => $t('Vouchers', ['scope' => 'ecommerce_voucher']),
 	'url' => ['controller' => 'Vouchers', 'action' => 'index', 'library' => 'ecommerce_voucher', 'admin' => true],
-	'weight' => 50
+	'weight' => 5
 ]);
 
 ?>
